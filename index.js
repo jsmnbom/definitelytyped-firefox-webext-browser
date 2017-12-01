f412d4926436155784c44d8fab0594d2ced3f8de
Jacob Bom <bomjacob@gmail.com>
2017-12-01 21:24:59 +0100

    Adding comments part 1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
-Requires firefox source code to be downloaded.
+Requires firefox source code to be downloaded, which can be found at https://archive.mozilla.org/pub/firefox/releases/ in the source subdirectory
 Use as:
 node index.js -f <FIREFOX VERSION> -s <SCHEMAS1> -s <SCHEMAS2> -o <OUTPUT_FILE>
 Where SCHEMAS are toolkit/components/extensions/schemas and
@@ -19,14 +19,19 @@ const argv = require("minimist")(process.argv.slice(2), {
     string: ['f']
 });
 
+// Reversed keywords in typescript
 const RESERVED = ["break", "case", "catch", "class", "const", "continue", "debugger", "default", "delete", "do", "else",
     "enum", "export", "extends", "false", "finally", "for", "function", "if", "import", "in", "instanceof", "new", "null",
     "return", "super", "switch", "this", "throw", "true", "try", "typeof", "var", "void", "while", "with"];
 
+// Namespace references that need renaming
 const NAMESPACE_ALIASES = {contextMenusInternal: 'menusInternal'};
+// Don't generate definitions for these namespaces
 const EXCLUDED_NAMESPACES = ['test'];
+// Types that are considered "simple"
 const SIMPLE_TYPES = ['string', 'integer', 'number', 'Date', 'boolean', 'any'];
 
+// Header of the definitions file
 const HEADER = `// Type definitions for WebExtension Development in FireFox ${argv['f']}
 // Project: https://developer.mozilla.org/en-US/Add-ons/WebExtensions
 // Definitions by: Jacob Bom <https://github.com/bomjacob>
@@ -44,15 +49,23 @@ interface EventListener<T extends (...args: any[]) => any> {
 
 class Converter {
     constructor(folders) {
+        // Generated source
         this.out = HEADER;
 
+        // Collect schema files
         this.schemaData = [];
         this.collectSchemas(folders);
 
+        // Convert from split schemas to namespace
+        // This merges all the properties that we care about for each namespace
+        // Needed since many schema files add to the "manifest" namespace
         this.namespaces = {};
         for (let data of this.schemaData) {
+            // Enumerate the actual namespace data
             for (let namespace of data[1]) {
+                // Don't add to this.namespaces if we should exclude it
                 if (EXCLUDED_NAMESPACES.includes(namespace.namespace)) continue;
+                // If we haven't seen this namespace before, init it
                 if (!this.namespaces.hasOwnProperty(namespace.namespace)) {
                     this.namespaces[namespace.namespace] = {
                         namespace: namespace.namespace,
@@ -62,6 +75,7 @@ class Converter {
                         events: []
                     };
                 }
+                // Concat or extend namespace
                 if (namespace.types) this.namespaces[namespace.namespace].types = this.namespaces[namespace.namespace].types.concat(namespace.types);
                 if (namespace.properties) this.namespaces[namespace.namespace].properties = Object.assign(this.namespaces[namespace.namespace].properties, namespace.properties);
                 if (namespace.functions) this.namespaces[namespace.namespace].functions = this.namespaces[namespace.namespace].functions.concat(namespace.functions);
@@ -71,6 +85,7 @@ class Converter {
     }
 
     convert() {
+        // For each namespace, set it as current, and convert it, which adds directly onto this.out
         for (let namespace of Object.keys(this.namespaces)) {
             this.namespace = namespace;
             this.convertNamespace();
@@ -78,10 +93,12 @@ class Converter {
     }
 
     collectSchemas(folders) {
+        // For each schema file
         for (let folder of folders) {
             const files = fs.readdirSync(folder);
             for (let file of files) {
                 if (path.extname(file) === '.json') {
+                    // Strip json comments, parse and add to data array
                     this.schemaData.push([file, JSON.parse(stripJsonComments(String(fs.readFileSync(path.join(folder, file)))))]);
                 }
             }
@@ -97,6 +114,7 @@ class Converter {
     }
 
     convertClass(type) {
+        // Convert each property, function and event of a class
         let out = `{\n`;
         let convertedProperties = this.convertObjectProperties(type);
         if (type.functions) for (let func of type.functions) {
@@ -113,16 +131,25 @@ class Converter {
 
     convertObjectProperties(type) {
         let convertedProperties = [];
+        // For each simple property
         if (type.properties) {
             for (let name of Object.keys(type.properties)) {
                 let propertyType = type.properties[name];
+                // Make sure it has a proper id by adding parent id to id
                 propertyType.id = type.id + (name === 'properties' ? '' : ('_' + name));
+                // Output property type (adding a ? if optional)
                 convertedProperties.push(`${name}${type.properties[name].optional ? '?' : ''}: ${this.convertType(propertyType)}`);
             }
-        } else if (type.patternProperties) {
+        }
+        // For each pattern property
+        if (type.patternProperties) {
             for (let name of Object.keys(type.patternProperties)) {
+                // Assume it's a string type
                 let keyType = 'string';
+                // TODO: Simple regex check, probably flawed
+                // If the regex has a \d and not a a-z, assume it's asking for a number
                 if (name.includes('\\d') && !name.includes('a-z')) keyType = 'number';
+                // Add the keyed property
                 convertedProperties.push(`[key: ${keyType}]: ${this.convertType(type.patternProperties[name])}`);
             }
         }
@@ -130,33 +157,43 @@ class Converter {
     }
 
     convertRef(ref) {
-        let out = '';
+        // Get the namespace of the reference, if any
         let namespace = ref.split('.')[0];
+        // Do we have an alias for that namesapce?
         if (NAMESPACE_ALIASES.hasOwnProperty(namespace)) {
+            // Revolve namespace aliases
             namespace = NAMESPACE_ALIASES[namespace];
             ref = `${namespace}.${ref.split('.')[1]}`
         }
+        // The namespace is unnecessary if it's the current one
         if (namespace === this.namespace) {
             ref = ref.split('.')[1];
         }
+        // If we know about the namespace
         if (Object.keys(this.namespaces).includes(namespace)) {
+            // Add browser. to the front
+            // Okay, apparently typescript doesn't need that, as all the namepaces are combined by the compiler
             //out += 'browser.';
         } else if (!this.namespaces[this.namespace].types.find(x => x.id === ref)) {
             console.log(`Warning: Cannot find reference "${ref}", assuming the browser knows better.`);
+            // Add a type X = any, so the type can be used, but won't be typechecked
             this.additionalTypes.push(`type ${ref} = any;`);
         }
-        out += ref;
-        return out;
+        return ref;
     }
 
     // noinspection JSMethodCanBeStatic
     convertEnumName(name) {
+        // Convert from snake_case to PascalCase
         return name.split('_').map(x => x.charAt(0).toUpperCase() + x.slice(1)).join('');
     }
 
     convertType(type, root = false) {
         let out = '';
+        // Check type of type
         if (type.choices) {
+            // Okay so it's a choice between several types, we need to check
+            // if choices include enums, and if so combine them
             let choices = [];
             let enums = [];
             for (let choice of type.choices) {
@@ -166,26 +203,36 @@ class Converter {
                     choices.push(choice)
                 }
             }
+            // If we found enums, output it as a single choice
             if (enums.length > 0) choices.push({
                 id: type.id,
                 enum: enums
             });
-            let z = choices.map(x => {
+            // For each choice, convert according to rules, join via a pipe "|" and add to output
+            out += choices.map(x => {
+                // Override id with parent id for proper naming
                 x.id = type.id;
+                // Convert it as a type
                 let y = this.convertType(x);
+                // If it's any, make it object instead and hope that works
+                // This is due to how "string | any" === "any" and the whole choice would therefore be redundant
                 if (y === 'any') y = 'object';
-                if (y === '() => void') y = 'Function';
-                if (y === type.id) y = '__ABORT__';
                 return y;
             }).join(' | ');
-            if (z.includes('__ABORT__')) return; // TODO: FIX THIS SHIT
-            out += z;
         } else if (type.enum) {
+            // If it's an enum
+            // Make sure it has a proper id
             if (type.name && !type.id) type.id = type.name;
+            // We can only output enums in the namespace root (a schema enum, instead of e.g. a property having an enum as type)
             if (root) {
+                // So if we are in the root
+                // Add each enum value, sanitizing the name (if it has one, otherwise just using its value as name)
                 out += `{\n${type.enum.map(x => `${(x.name ? x.name : x).replace(/\W/g, '')} = "${x.name ? x.name : x}"`).join(',\n')}\n}`
             } else {
+                // If we're not in the root, add the enum as an additional type instead, adding an _ in front of the name
+                // We convert the actual enum based on rules above by passing through the whole type code again, but this time as root
                 this.additionalTypes.push(`enum _${this.convertEnumName(type.id)} ${this.convertType(type, true)}`);
+                // And then just reference it by name in output
                 out += '_' + this.convertEnumName(type.id);
             }
         } else if (type.type) {
@@ -399,17 +446,22 @@ class Converter {
     }
 
     convertNamespace() {
+        // Get data for this namespace
         let data = this.namespaces[this.namespace];
         let out = '';
 
+        // Clear additional types
         this.additionalTypes = [];
+        // Convert everything
         this.types = this.convertTypes(data.types);
         this.properties = this.convertProperties(data.properties);
         this.functions = this.convertFunctions(data.functions);
         this.events = this.convertEvents(data.events);
 
+        // Make sure there are no duplicates
         this.additionalTypes = _.uniqWith(this.additionalTypes, _.isEqual);
 
+        // Output everything if needed
         out += `declare namespace browser.${data.namespace} {\n`;
         if (this.types.length > 0) out += `/* ${data.namespace} types */\n${this.types.join('\n\n')}\n\n`;
         if (this.additionalTypes.length > 0) out += `${this.additionalTypes.join('\n\n')}\n\n`;
@@ -422,7 +474,9 @@ class Converter {
     }
 
     write(filename) {
+        // Delete file
         fs.truncate(filename, 0, function () {
+            // Write this.out to file except the very last character (which is an extra \n)
             fs.writeFileSync(filename, this.out.slice(0, this.out.length - 1));
         }.bind(this));
     }
