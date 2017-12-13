47b941e8163fe78717c2d7164fe8305595bb27ca
Jacob Bom <bomjacob@gmail.com>
2017-12-13 14:19:17 +0100

    Allow Promise<void> if void is only type

--- converter.js
+++ converter.js
@@ -539,7 +539,7 @@ class Converter {
             }
             // Use void as return type if there were no parameters
             // Note that the join is kinda useless (see long comments above)
-            let promiseReturn = parameters[0] || 'undefined';
+            let promiseReturn = parameters[0] || 'void';
             if (callback.optional && !ALREADY_OPTIONAL_RETURNS.includes(promiseReturn)) promiseReturn += ' | undefined';
             returnType = `Promise<${promiseReturn}>`
         }
